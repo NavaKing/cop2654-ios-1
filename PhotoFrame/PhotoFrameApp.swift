631ae13cfba0c19fe843e58e2a26cb21a6fbdeab
Giovanni Nava <ngiovannie@outlook.com>
2023-08-28 20:25:06 -0400

    Initial Commit

---
+++ PhotoFrame/PhotoFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoFrameApp.swift
+//  PhotoFrame
+//
+//  Created by Jarvis 2.0 on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
